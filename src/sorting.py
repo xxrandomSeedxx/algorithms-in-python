18a6197188e34b184a0c36f09c081c1cb712179b
Lam <shun_min@hotmail.com>
2023-01-11 13:32:42 +0800

    -update quick sort

--- src/sorting.py
+++ src/sorting.py
@@ -1,15 +1,17 @@
 # Quick sort
-a = [1, 12, 5, 16, 10, 20]
 
-# partitioning
-pivot = a[-1]
-left = 0
-right = len(a) - 1
-for i in range(left, right + 1):
-    if left < pivot:
-        continue
-    left = i
-for j in range(right, 0, -1):
-    if right > pivot:
-        continue
-    right = j
+def partition(low, high):
+    pivot = nums[-1]
+    i = low - 1
+    for j in range(low, high):
+        if nums[j] > pivot:
+            continue
+        i
+
+
+def quicksort(low, high):
+    # partitioning
+    p1 = partition(low, high)
+
+nums = [1, 12, 5, 16, 10, 20]
+quicksort(0, len(nums) - 1)
